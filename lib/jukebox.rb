7cd9e21fca76fa98b17b54727dbe6562e5f48e47
jules blanc <jules.blanc@unccub.com>
2019-10-06 22:31:52 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -11,4 +11,11 @@
     "Amos Lee - Keep It Loose, Keep It Tight"
   ]
   
-  
+def say_hello(name)
+  "Hi #{name}!"
+end
+     
+puts "Enter your name:"
+users_name = gets.strip
+     
+puts say_hello(users_name)
